f7419b010fee072860f0b79d0038589357a85fc7
nafedev <aquaroleplayhelp@gmail.com>
2023-09-20 12:49:35 +0100

    add(baston) + walking stick script

---
+++ baston/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'adamant'
+
+game 'gta5'
+author '! n#0069'
+
+server_scripts {'server.lua'}
+
+client_scripts {'client.lua'}
---
+++ baston/server.lua
@@ -0,0 +1,4 @@
+
+RegisterCommand("baston", function(source) 
+    TriggerClientEvent('stg_walkingstick', source)
+end) -- baston command - uses walking stick
