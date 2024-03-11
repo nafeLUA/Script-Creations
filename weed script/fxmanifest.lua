1860ad0757aa2d5d2633b75dd99f1a5a69ce7a26
Nafe <133820318+nafeLUA@users.noreply.github.com>
2024-03-11 23:52:16 +0000

    add(weedjob) + whitewidow job for qbcore

---
+++ weed script/fxmanifest.lua
@@ -0,0 +1,23 @@
+
+
+fx_version 'adamant'
+
+game 'gta5'
+
+author 'nafe'
+
+description 'whitewidow job made by nafe'
+
+version '1.0'
+
+shared_scripts {
+    'config.lua',
+}
+
+client_scripts{
+    'client/*.lua',
+}
+
+server_scripts{
+    'server/*.lua',
+}
